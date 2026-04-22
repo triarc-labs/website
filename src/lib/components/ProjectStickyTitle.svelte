51fd34b17e96b9a0c1c68386e9fc8a135531e776
nocrest <lukihartmann@bluewin.ch>
2026-04-22 15:27:59 +0200

    fix: sticky title alignment and icon scaling

--- src/lib/components/ProjectStickyTitle.svelte
+++ src/lib/components/ProjectStickyTitle.svelte
@@ -10,23 +10,36 @@
 <div
   class="
   {scrolled ? 'scrolled' : ''}
-  md:sticky md:top-16 flex z-10 flex-col md:flex-row gap-y-2 gap-x-4 md:items-center bg-transparent py-8"
+  md:sticky md:top-16 flex z-10 flex-col md:flex-row gap-y-2 md:items-center bg-transparent py-8"
 >
-  <Icon class="lg:hidden inline" size="medium" color={iconColor} src={iconSrc} />
-  <Icon class="hidden lg:inline" size="large" color={iconColor} src={iconSrc} />
-  <span id="sticky-title" class="project-title text-2xl lg:text-5xl text-white font-bold text-balance">{title}</span>
+  <Icon
+    class="icon lg:hidden self-start mr-10 flex-shrink-0 origin-top-left transition-transform ease-in-out duration-300"
+    size="medium"
+    color={iconColor}
+    src={iconSrc}
+  />
+  <Icon
+    class="icon hidden lg:block self-start mr-10 flex-shrink-0 origin-top-left transition-transform ease-in-out duration-300"
+    size="large"
+    color={iconColor}
+    src={iconSrc}
+  />
+  <span
+    id="sticky-title"
+    class="project-title origin-top-left transition-transform ease-in-out duration-300 text-2xl lg:text-5xl leading-tight text-white font-bold text-balance"
+    >{title}</span
+  >
 </div>
 
 <style>
-  .project-title {
-    @apply inline-block origin-left transition-transform ease-in-out duration-300;
-    /*transform-origin: left center;*/
-    /*transition: transform 330ms cubic-bezier(0.2, 0.9, 0.3, 1);*/
-  }
-
   .scrolled .project-title {
     @apply md:scale-75;
   }
+
+  .scrolled :global(.icon) {
+    @apply md:scale-75;
+  }
+
   .scrolled {
     @apply text-3xl;
   }
